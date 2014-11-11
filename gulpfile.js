bfd794cdf3d4ff3a54cae9f8b52996147f664100
jaxgeller <jacksongeller@gmail.com>
2014-11-11 11:40:06 -0500

    added gulp micro-optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,34 @@
 var gulp = require("gulp");
-var plugin = require('gulp-load-plugins')();
+var p = require('gulp-load-plugins')();
 
 var handle = function(err) {console.log(err); this.emit('end');}
 
 
 gulp.task('sass', function() {
   return gulp.src('src/style/style.scss')
-    .pipe(plugin.sass({outputStyle:'compressed'}))
+    .pipe(p.sass({outputStyle:'compressed'}))
     .on('error', handle)
-    .pipe(gulp.dest('public/'));
+    .pipe(gulp.dest('public'));
 });
 
 gulp.task('scripts', function() {
   return gulp.src('src/scripts/*.js')
-    .pipe(plugin.concat('scripts.js'))
-    .pipe(plugin.uglify())
+    .pipe(p.concat('scripts.js'))
+    .pipe(p.uglify())
     .on('error', handle)
-    .pipe(gulp.dest('public/'))
+    .pipe(gulp.dest('public'))
 });
 
 gulp.task('ejs', function() {
   return gulp.src('src/views/*.ejs')
-    .pipe(plugin.ejs())
+    .pipe(p.ejs())
     .on('error', handle)
-    .pipe(plugin.htmlmin({collapseWhitespace: true})) 
-    .pipe(gulp.dest('./public'));
+    .pipe(p.htmlmin({collapseWhitespace: true})) 
+    .pipe(gulp.dest('public'));
 });
 
 gulp.task('server', function() {
-  return plugin.connect.server({
+  return p.connect.server({
     root: 'public',
     port: 8000
   });
