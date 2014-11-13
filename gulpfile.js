4087e44f5921eec3f830f5ee5f4871d48098d72f
jaxgeller <jacksongeller@gmail.com>
2014-11-13 13:42:04 -0500

    reordered gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,22 @@ var p = require('gulp-load-plugins')();
 
 var handle = function(err) { console.log(err); this.emit('end'); }
 
+gulp.task('server', function() {
+  return p.connect.server({
+    root: 'public',
+    port: 8000
+  });
+});
+
+gulp.task('ejs', function() {
+  return gulp.src('src/views/*.ejs')
+    .pipe(p.ejs())
+    .on('error', handle)
+    .pipe(p.htmlmin({collapseWhitespace: true, removeComments: true}))
+    .pipe(gulp.dest('public'));
+});
+
+
 gulp.task('sass', function() {
   return gulp.src('src/style/style.scss')
     .pipe(p.sass({outputStyle: 'compressed'}))
@@ -18,25 +34,13 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('public'))
 });
 
-gulp.task('ejs', function() {
-  return gulp.src('src/views/*.ejs')
-    .pipe(p.ejs())
-    .on('error', handle)
-    .pipe(p.htmlmin({collapseWhitespace: true, removeComments: true}))
-    .pipe(gulp.dest('public'));
-});
 
-gulp.task('server', function() {
-  return p.connect.server({
-    root: 'public',
-    port: 8000
-  });
-});
+
 
 gulp.task('watch', function() {
+  gulp.watch('src/views/**/*.ejs', ['ejs']);
   gulp.watch('src/style/*.scss', ['sass']);
   gulp.watch('src/scripts/*.js', ['scripts']);
-  gulp.watch('src/views/**/*.ejs', ['ejs']);
 });
 
 gulp.task('default', [ 'server','ejs', 'sass', 'scripts', 'watch']);
