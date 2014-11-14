c01fceff2ea777a38189469370d31b22103c2ad3
Michael Cavalea <callmecavs@gmail.com>
2014-11-14 16:27:55 -0500

    add and style footer easter egg

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){console.log("Looking for this? https://github.com/BraveUX/website")});
+function initEasterEggs(){$(".footer-copyright-stars").click(function(){$("footer").velocity("scroll",{duration:500,axis:"y",begin:function(){$("footer").addClass("is-egg")}})})}$(document).ready(function(){console.log("Looking for this? https://github.com/BraveUX/website"),initEasterEggs()});
--- src/scripts/ready.js
+++ src/scripts/ready.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
   console.log('Looking for this? https://github.com/BraveUX/website');
+
+  initEasterEggs();
 });
+
+function initEasterEggs() {
+  $('.footer-copyright-stars').click(function() {
+    // scroll to bottom
+    $('footer').velocity('scroll', { duration: 500, axis: 'y', begin: function() {
+      // show easter egg
+      $('footer').addClass('is-egg');
+    }});
+  });
+}
