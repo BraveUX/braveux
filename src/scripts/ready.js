9093545016e323657b61cb8c1bfc7a4e28cbbc12
Michael Cavalea <callmecavs@gmail.com>
2014-11-17 11:30:00 -0500

    add scrollReveal.js, half blocks to Fieldone case

--- src/scripts/ready.js
+++ src/scripts/ready.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   console.log('Looking for this? https://github.com/BraveUX/website');
 
+  initScrollSpy();
   initEasterEggs();
 });
 
@@ -11,5 +12,18 @@ function initEasterEggs() {
       // show easter egg
       $('footer').addClass('is-egg');
     }});
+  });
+}
+
+function initScrollSpy() {
+  window.sr = new scrollReveal({
+    move: '0px',
+    over: '0.5s',
+    wait: '0s',
+    easing: 'ease',
+    mobile: true,
+    delay: 'once',
+
+    vFactor: 0.30     // 70% of element onscreen to trigger reveal
   });
 }
