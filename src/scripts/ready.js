6b1a71fac2c287ee8a7c785f453d47a2fb2171fd
jaxgeller <jacksongeller@gmail.com>
2014-11-20 11:48:25 -0500

    slight refactor

--- src/scripts/ready.js
+++ src/scripts/ready.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
-  console.log('Looking for this? https://github.com/BraveUX/website');
-
   typewriter('.typewriter');
-
   initScrollSpy();
   initEasterEggs();
+  console.log('Looking for this? https://github.com/BraveUX/website');
 });
 
 function initEasterEggs() {
