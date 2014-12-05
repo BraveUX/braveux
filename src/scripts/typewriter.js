7838eaca6265443f22cdb22d20ad5434248ab4ca
Michael Cavalea <callmecavs@gmail.com>
2014-12-04 22:58:45 -0500

    fix typewriter phrase order, cleanup spacing (@jaxgeller great work, terrible spacing - classic)

--- src/scripts/typewriter.js
+++ src/scripts/typewriter.js
@@ -1,39 +1,39 @@
 (function($) {
-
-
-  var test = [
-    {
-      selector: $('.typewriter-1'),
-      words: ['happy users.', 'better user flow.', 'to understand your users.', 'user experience.']
-    }, {
-      selector: $('.typewriter-2'),
-      words: ['designing awesome apps.', 'smart and capable.', 'research-driven.', 'Brave.']
-    }
-];
+  var test = [ {
+    selector: $('.typewriter-1'),
+    words: ['happy users.', 'better user flow.', 'to understand your users.', 'user experience.']
+  }, {
+    selector: $('.typewriter-2'),
+    words: ['designing awesome apps.', 'smart and capable.', 'research-driven.', 'Brave.']
+  }];
 
   $.fn.typewriter = function() {
+    var iterator = 0;
+
     var current = {
       selector: test[0].selector,
       phrase: test[0].words,
       word: test[0].words[0].split('')
     }
-    var iterator = 0;
 
     function type (word) {
       current.selector.siblings('.typewriter-cursor').text('|');
+
       if (word.length) {
         current.selector.text(current.selector.text() + word[0])
         word.shift();
         setTimeout(type, 80, word);
-      } else {
-
+      }
+      else {
         setTimeout(function() {
           current.selector.siblings('.typewriter-cursor').text('');
           iterator ++;
+
           // if second, start highlights
           if (iterator % 2 === 0) {
             // start first highlight
             current.selector.addClass('typewriter-highlight');
+
             setTimeout(function() {
               current.selector.text('');
               current.selector.removeClass('typewriter-highlight');
@@ -45,15 +45,17 @@
               test[1].selector.removeClass('typewriter-highlight');
               current.phrase.push(current.phrase.shift());
               test.push(test.shift());
+
               current = {
                 selector: test[0].selector,
                 phrase: test[0].words,
                 word: test[0].words[0].split('')
               }
-              type(current.word)
-            }, 2000);
 
-          } else {
+              type(current.word);
+            }, 2000);
+          }
+          else {
             // swap current phrases
             current.phrase.push(current.phrase.shift());
 
@@ -66,11 +68,13 @@
               phrase: test[0].words,
               word: test[0].words[0].split('')
             }
-            type(current.word)  
+
+            type(current.word);
           }
         }, 1000);
       }
     }
+
     type(current.word);
   };
 
