7cacb6d2221bdcc6545f5c1c88d186525b37e0bd
Michael Cavalea <callmecavs@gmail.com>
2014-12-12 13:49:39 -0500

    refactor everything for new file structure (add gulp FTP config as well)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,20 @@
 var gulp = require('gulp');
 var p = require('gulp-load-plugins')();
 
-var handle = function(err) { console.log(err); this.emit('end'); }
+var handle = function(err) {
+  console.log(err); this.emit('end');
+}
+
+var ftpSetup = function(path) {
+  path = path || '';
+
+  return {
+    host: 'braveux.com',
+    user: process.env.ftp_username,
+    pass: process.env.ftp_password,
+    remotePath: '/public_html/staging/' + path
+  }
+}
 
 gulp.task('server', function() {
   return p.connect.server({
@@ -16,12 +29,19 @@ gulp.task('images', function() {
     .pipe(gulp.dest('public/images'));
 });
 
+gulp.task('fonts', function() {
+  return gulp.src('src/fonts/**/*')
+    // .pipe(p.ftp(ftpSetup('fonts')))
+    .pipe(gulp.dest('public/fonts'));
+});
+
 gulp.task('ejs', function() {
-  return gulp.src('src/views/*.ejs')
+  return gulp.src('src/views/**/*.ejs')
     .pipe(p.ejs())
     .on('error', handle)
     .pipe(p.htmlmin({collapseWhitespace: true, removeComments: true}))
     .on('error', handle)
+    // .pipe(p.ftp(ftpSetup()))
     .pipe(gulp.dest('public'));
 });
 
@@ -29,6 +49,7 @@ gulp.task('sass', function() {
   return gulp.src('src/style/style.scss')
     .pipe(p.sass({outputStyle: 'compressed'}))
     .on('error', handle)
+    // .pipe(p.ftp(ftpSetup()))
     .pipe(gulp.dest('public'));
 });
 
@@ -37,14 +58,16 @@ gulp.task('scripts', function() {
     .pipe(p.concat('scripts.js'))
     .pipe(p.uglify())
     .on('error', handle)
-    .pipe(gulp.dest('public'))
+    // .pipe(p.ftp(ftpSetup()))
+    .pipe(gulp.dest('public'));
 });
 
 gulp.task('watch', function() {
   gulp.watch('src/images/**/*', ['images']);
+  gulp.watch('src/fonts/**/*', ['fonts']);
   gulp.watch('src/views/**/*.ejs', ['ejs']);
   gulp.watch('src/style/*.scss', ['sass']);
   gulp.watch('src/scripts/*.js', ['scripts']);
 });
 
-gulp.task('default', [ 'server','images', 'ejs', 'sass', 'scripts', 'watch']);
+gulp.task('default', [ 'server', 'images', 'fonts', 'ejs', 'sass', 'scripts', 'watch' ]);
