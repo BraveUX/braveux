feca30023865b69262411effa8d6f5edf93b7b62
jaxgeller <jacksongeller@gmail.com>
2015-01-14 13:36:39 -0500

    add notifications on errors, and added livereload

--- gulpfile.js
+++ gulpfile.js
@@ -5,61 +5,51 @@ var handle = function(err) {
   console.log(err); this.emit('end');
 }
 
-var ftpSetup = function(path) {
-  path = path || '';
-
-  return {
-    host: 'braveux.com',
-    user: process.env.ftp_username,
-    pass: process.env.ftp_password,
-    remotePath: '/public_html/staging/' + path
-  }
-}
-
 gulp.task('server', function() {
   return p.connect.server({
     root: 'public',
-    port: 8000
+    port: 8000,
+    livereload: true
   });
 });
 
 gulp.task('images', function() {
   return gulp.src('src/images/**/*')
     .pipe(p.imagemin({progressive: true}))
+    .on('error', p.notify.onError('Error: <%= error.message %>'))
     .pipe(gulp.dest('public/images'));
 });
 
 gulp.task('fonts', function() {
   return gulp.src('src/fonts/**/*')
-    // .pipe(p.ftp(ftpSetup('fonts')))
     .pipe(gulp.dest('public/fonts'));
 });
 
 gulp.task('ejs', function() {
   return gulp.src('src/views/**/*.ejs')
     .pipe(p.ejs())
-    .on('error', handle)
+    .on('error', p.notify.onError('Error: <%= error.message %>'))
     .pipe(p.htmlmin({collapseWhitespace: true, removeComments: true}))
-    .on('error', handle)
-    // .pipe(p.ftp(ftpSetup()))
-    .pipe(gulp.dest('public'));
+    .pipe(gulp.dest('public'))
+    .pipe(p.connect.reload());
 });
 
 gulp.task('sass', function() {
   return gulp.src('src/style/style.scss')
     .pipe(p.sass({outputStyle: 'compressed'}))
+    .on('error', p.notify.onError('Error: <%= error.message %>'))
     .on('error', handle)
-    // .pipe(p.ftp(ftpSetup()))
-    .pipe(gulp.dest('public'));
+    .pipe(gulp.dest('public'))
+    .pipe(p.connect.reload());
 });
 
 gulp.task('scripts', function() {
   return gulp.src('src/scripts/*.js')
     .pipe(p.concat('scripts.js'))
     .pipe(p.uglify())
-    .on('error', handle)
-    // .pipe(p.ftp(ftpSetup()))
-    .pipe(gulp.dest('public'));
+    .on('error', p.notify.onError('Error: <%= error.message %>'))
+    .pipe(gulp.dest('public'))
+    .pipe(p.connect.reload());
 });
 
 gulp.task('watch', function() {
@@ -70,4 +60,4 @@ gulp.task('watch', function() {
   gulp.watch('src/scripts/*.js', ['scripts']);
 });
 
-gulp.task('default', [ 'server', 'fonts', 'ejs', 'sass', 'scripts', 'watch' ]);
+gulp.task('default', [ 'server', 'fonts', 'images', 'ejs', 'sass', 'scripts', 'watch' ]);
