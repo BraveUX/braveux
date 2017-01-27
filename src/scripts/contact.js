f6cd3b065b32cce7a16890502f722cbd38c64cf8
Eric Fuhrmann <ekfuhrmann@gmail.com>
2017-01-27 16:13:16 -0500

    Updated Contact page to v1.5 (temp)

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -3,11 +3,17 @@
 // Contact Letter Flip
 $(function() {
   var letter = $('.contact-letter'); // Get Letter
-  var windowHeight = $(window).height()/8; // Flip at 1/8 window
+  var letterDistance = $('.contact-letter').offset().top;
 
+  // Update var if window resizes
+  $(window).resize(function() {
+    letterDistance = $('.contact-letter').offset().top;
+  });
+
+  // Flip card when scrolled into viewport
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
-    if (scroll >= windowHeight) {
+    if (scroll >= letterDistance/1.7) {
       letter.addClass('is-flipped');
     } else {
       letter.removeClass('is-flipped');
@@ -16,120 +22,91 @@ $(function() {
 });
 
 // Contact Letter Label Control
-$(function() {
-  $('input, textarea').each(function() {
-    $(this).blur('data', function() {
-      var checkValue = $(this).val();
-      if (checkValue !== '') {
-        $(this).next('label').addClass('has-content');
-      } else {
-        $(this).next('label').removeClass('has-content');
-      }
-    });
-  });
-});
+// $(function() {
+//   $('input, textarea').each(function() {
+//     $(this).blur('data', function() {
+//       var checkValue = $(this).val();
+//       if (checkValue !== '') {
+//         $(this).next('label').addClass('has-content');
+//       } else {
+//         $(this).next('label').removeClass('has-content');
+//       }
+//     });
+//   });
+// });
 
 // Contact Letter Paging & Validation
-$(function() {
-  var form = $('.contact-form');
-
-  // Buttons
-  var pageOneBtn = $('#page1 .contact-form-cta');
-  var pageTwoBtn = $('#page2 .contact-form-cta');
-  var pageThreeBtn = $('#page3 .contact-form-cta');
-
-  // Fields
-  var name = document.getElementById('user-name');
-  var errorName = $('.error-name');
-  var org = document.getElementById('organization');
-  var errorOrg = $('.error-org');
-  var comment = document.getElementById('comment');
-  var errorComment = $('.error-comment');
-  var email = document.getElementById('email');
-  var errorEmail = $('.error-email');
-  var tel = document.getElementById('telephone');
-  var errorTel = $('.error-tel');
-
-  // Page 1
-  $(pageOneBtn).click(function(e) {
-    if (!name.validity.valid && !org.validity.valid) {
-      e.preventDefault();
-      errorName.addClass('is-active');2
-      errorOrg.addClass('is-active');
-    } else if (!name.validity.valid) {
-      e.preventDefault();
-      errorOrg.removeClass('is-active');
-      errorName.addClass('is-active');
-    } else if (!org.validity.valid) {
-      e.preventDefault();
-      errorName.removeClass('is-active');
-      errorOrg.addClass('is-active');
-    } else {
-      var nameVal = $('#user-name').val(); // Get Value
-      errorName.removeClass('is-active');
-      errorOrg.removeClass('is-active');
-      $('#page2 .contact-form-text').html('Hello ' + nameVal + '! How can we help?');
-      $('#page1').css('visibility', 'hidden');
-      $('#page2').css('visibility', 'visible');
-    }
-  });
-
-  // Page 2
-  $(pageTwoBtn).click(function(e) {
-    if (!comment.validity.valid) {
-      e.preventDefault();
-      errorComment.addClass('is-active');
-    } else {
-      errorComment.removeClass('is-active');
-      $('#page2').css('visibility', 'hidden');
-      $('#page3').css('visibility', 'visible');
-    }
-  });
-
-  // Page 3
-  $(pageThreeBtn).click(function(e) {
-    if (!email.validity.valid && !tel.validity.valid) {
-      e.preventDefault();
-      errorEmail.addClass('is-active');
-      errorTel.addClass('is-active');
-    } else if (!email.validity.valid) {
-      e.preventDefault();
-      errorEmail.addClass('is-active');
-    } else if (!tel.validity.valid) {
-      e.preventDefault();
-      errorEmail.removeClass('is-active');
-      errorTel.addClass('is-active');
-    } else {
-      e.preventDefault();
-      errorEmail.removeClass('is-active');
-      errorTel.removeClass('is-active');
-      $('#page3').css('visibility', 'hidden');
-      $('#page4').css('visibility', 'visible');
-    }
-  });
-
-  // Paging Dots
-  $('#page2 .paging-circle:nth-of-type(1)').click(function(e) { // Page 2
-    e.preventDefault();
-    $('#page2').css('visibility', 'hidden');
-    $('#page1').css('visibility', 'visible');
-    $
-  })
-  $('#page3 .paging-circle:nth-of-type(1)').click(function(e) { // Page 3
-    e.preventDefault();
-    $('#page3').css('visibility', 'hidden');
-    $('#page1').css('visibility', 'visible');
-    $
-  })
-  $('#page3 .paging-circle:nth-of-type(2)').click(function(e) { // Page 3
-    e.preventDefault();
-    $('#page3').css('visibility', 'hidden');
-    $('#page2').css('visibility', 'visible');
-    $
-  })
-});
-
-
-var formSteps =
-
-[['#page1', {this:that}], ['#page1', {this:that}]]
+// $(function() {
+//   var form = $('.contact-form');
+//   var stepIndex = 0;
+//   var formSteps = {
+//     // input : error
+//     page1 : {
+//       'user-name' : 'error-name',
+//       'organization' : 'error-org'
+//     },
+//     page2 : {
+//       'comment' : 'error-comment',
+//     },
+//     page3 : {
+//       'email' : 'error-email',
+//       'telephone' : 'error-tel'
+//     }
+//   };
+//
+//   formSteps.each(function(key, value) {
+//     // key = page | value = array
+//     var $pageBtn = $('#' + key + ' .contact-form-cta').click(function(e) {
+//       validateFormFields(value);
+//     });
+//   });
+//
+//   function configurePager() {
+//     $('.contact-form-paging').each(function(e) {
+//       $pagers = $(this).children('.paging-circle');
+//       $pagers.each(function(index, value) {
+//         $pager = $(this);
+//         $pager.onclick (function(e) {
+//           if((index + 1) < stepIndex) {
+//             gotoFormStep(index + 1);
+//           }
+//         });
+//       });
+//     });
+//   }
+//
+//   function validateFormFields(formFields) {
+//     // validate fields based on incoming array
+//     var isValid = true;
+//
+//     formFields.each(obj, function(key, value) {
+//       var formField = document.getElementById(key);
+//
+//       if( !formField.validity.valid ) {
+//         // reveal error message
+//         $('.' + value).css('visibility','visible');
+//         isValid = false;
+//       }
+//     });
+//
+//     if(isValid) {
+//       gotoFormStep(stepIndex + 1);
+//     }
+//   }
+//
+//   function gotoFormStep(newIndex) {
+//     $step = getCurrentStep();
+//     // hide current step
+//     $step.css('visibility','hidden');
+//
+//     // update stepIndex
+//     stepIndex = newIndex;
+//
+//     // show next step
+//     $step.css('visibility','visible');
+//   }
+//
+//   function getCurrentStep(){
+//     return $('#' + Object.keys(formSteps)[stepIndex]);
+//   }
+// });
