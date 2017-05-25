fd6f89e4ab5706c0fe78fe48f0d85945bed50b66
Eric Fuhrmann <ekfuhrmann@gmail.com>
2017-05-25 11:49:29 -0400

    Major savings on repetitive code -- animations only play when in view

--- src/scripts/animations/approach.js
+++ src/scripts/animations/approach.js
@@ -32,10 +32,18 @@ window.addEventListener('resize', checkMediaWidth);
 function mediaCheck() {
   // Check if not on mobile
   if (! $('.menu-mobile-toggle').is(':visible') ) {
+    // Scene 1
     initAnimation('#animation-container-one');
+    sectionAnimation('one','libOne','scene1animationv2');
+    // Scene 2
     initAnimation('#animation-container-two');
+    sectionAnimation('two','libTwo','scene2animationv2');
+    // Scene 3
     initAnimation('#animation-container-three');
-    initAnimation('#animation-container-four');3
+    sectionAnimation('three','libThree','scene3animationv2');
+    // Scene 4
+    initAnimation('#animation-container-four');
+    sectionAnimation('four','libFour','scene4animationv2');
   }
 }
 
@@ -53,163 +61,121 @@ function ctaScroll() {
   });
 }
 
-var animationOneVisible, animationTwoVisible, animationThreeVisible, animationFourVisible = false;
-
 // Initialize Animation
 function initAnimation(id) {
-  $(id).waypoint(function() {
-    // Check ID and see whether it's visible
-    if ($(id + '[id*="one"]').length && !animationOneVisible) {
-      animationOneVisible = true;
-      animationOne();
-      console.log()
-    } else if ($(id + '[id*="two"]').length && !animationTwoVisible) {
-      animationTwoVisible = true;
-      animationTwo();
-    } else if ($(id + '[id*="three"]').length && !animationThreeVisible) {
-      animationThreeVisible = true;
-      animationThree();
-    } else if ($(id + '[id*="four"]').length && !animationFourVisible) {
-      animationFourVisible = true;
-      animationFour();
+  // Only plays animation when in viewport
+  new Waypoint.Inview({
+    element: $(id),
+    enter: function() {
+      if (id.indexOf('one') >= 0) {
+        resumeAnimation(0);
+        console.log('start animation' + id);
+      } else if (id.indexOf('two') >= 0) {
+        resumeAnimation(1);
+        console.log('start animation' + id);
+      } else if (id.indexOf('three') >= 0) {
+        resumeAnimation(2)
+        console.log('start animation' + id);
+      } else if (id.indexOf('four') >= 0) {
+        resumeAnimation(3)
+        console.log('start animation' + id);
+      }
+    },
+    exited: function() {
+      if (id.indexOf('one') >= 0) {
+        pauseAnimation(0);
+        console.log('end animation' + id);
+      } else if (id.indexOf('two') >= 0) {
+        pauseAnimation(1);
+        console.log('end animation' + id);
+      } else if (id.indexOf('three') >= 0) {
+        pauseAnimation(2)
+        console.log('end animation' + id);
+      } else if (id.indexOf('four') >= 0) {
+        pauseAnimation(3)
+        console.log('end animation' + id);
+      }
     }
-  }, {
-    offset: '100%'
   });
 }
 
-var canvas, stage, exportRoot, anim_container, dom_overlay_container, fnStartAnimation;
+// pause animations
+function pauseAnimation(index){
+  var stage = stages[index];
+  createjs.Ticker.removeEventListener('tick', stage);
+}
 
-//Code to support hidpi screens and responsive scaling.
-function makeResponsive(isResp, respDim, isScale, scaleType) {
-  var lastW, lastH, lastS = 1;
-  window.addEventListener('resize', resizeCanvas);
-  resizeCanvas();
+// resume animation
+function resumeAnimation(index){
+   var stage = stages[index];
+  createjs.Ticker.addEventListener('tick', stage);
+}
 
-  function resizeCanvas() {
-    var w = libOne.properties.width,
-      h = libOne.properties.height;
-    var iw = window.innerWidth,
-      ih = window.innerHeight;
-    var pRatio = window.devicePixelRatio || 1,
-      xRatio = iw / w,
-      yRatio = ih / h,
-      sRatio = 1;
-    if (isResp) {
-      if ((respDim == 'width' && lastW == iw) || (respDim == 'height' && lastH == ih)) {
-        sRatio = lastS;
-      } else if (!isScale) {
-        if (iw < w || ih < h)
-          sRatio = Math.min(xRatio, yRatio);
-      } else if (scaleType == 1) {
-        sRatio = Math.min(xRatio, yRatio);
-      } else if (scaleType == 2) {
-        sRatio = Math.max(xRatio, yRatio);
+// running list of all CreateJs Stage objects
+var stages = [];
+
+// index = 'one','two' etc
+// lib = libOne,libTwo, etc
+// scene = scene1animationv2, etc
+function sectionAnimation(index,libName,scene){
+  var lib = window[libName];
+  var canvas = document.getElementById('canvas-'+index);
+  var anim_container = document.getElementById('animation-container-'+index);
+  dom_overlay_container = document.getElementById('dom-overlay-container-'+index);
+  handleComplete();
+
+  function handleComplete() {
+    //This function is always called, irrespective of the content. You can use the variable 'stage' after it is created in token create_stage.
+    exportRoot = new window[libName][scene];
+    stage = new createjs.Stage(canvas);
+    stages.push(stage);
+    stage.addChild(exportRoot);
+
+    // 'tick' event listener handled by initAnimation()
+    fnStartAnimation = function() {
+      createjs.Ticker.setFPS(lib.properties.fps);
+    }
+
+    makeResponsive(true, 'both', true, 1);
+    fnStartAnimation();
+
+    //Code to support hidpi screens and responsive scaling.
+    function makeResponsive(isResp, respDim, isScale, scaleType) {
+      var lastW, lastH, lastS = 1;
+      window.addEventListener('resize', resizeCanvas);
+      resizeCanvas();
+
+      function resizeCanvas() {
+        var w = libOne.properties.width,
+          h = libOne.properties.height;
+        var iw = window.innerWidth,
+          ih = window.innerHeight;
+        var pRatio = window.devicePixelRatio || 1,
+          xRatio = iw / w,
+          yRatio = ih / h,
+          sRatio = 1;
+        if (isResp) {
+          if ((respDim == 'width' && lastW == iw) || (respDim == 'height' && lastH == ih)) {
+            sRatio = lastS;
+          } else if (!isScale) {
+            if (iw < w || ih < h)
+              sRatio = Math.min(xRatio, yRatio);
+          } else if (scaleType == 1) {
+            sRatio = Math.min(xRatio, yRatio);
+          } else if (scaleType == 2) {
+            sRatio = Math.max(xRatio, yRatio);
+          }
+        }
+        canvas.width = w * pRatio * sRatio;
+        canvas.height = h * pRatio * sRatio;
+        canvas.style.width = dom_overlay_container.style.width = anim_container.style.width = w * sRatio + 'px';
+        canvas.style.height = anim_container.style.height = dom_overlay_container.style.height = h * sRatio + 'px';
+        stage.scaleX = pRatio * sRatio;
+        stage.scaleY = pRatio * sRatio;
+        lastW = iw;
+        lastH = ih;
+        lastS = sRatio;
       }
     }
-    canvas.width = w * pRatio * sRatio;
-    canvas.height = h * pRatio * sRatio;
-    canvas.style.width = dom_overlay_container.style.width = anim_container.style.width = w * sRatio + 'px';
-    canvas.style.height = anim_container.style.height = dom_overlay_container.style.height = h * sRatio + 'px';
-    stage.scaleX = pRatio * sRatio;
-    stage.scaleY = pRatio * sRatio;
-    lastW = iw;
-    lastH = ih;
-    lastS = sRatio;
-  }
-}
-
-function animationOne() {
-  canvas = document.getElementById('canvas-one');
-  anim_container = document.getElementById('animation-container-one');
-  dom_overlay_container = document.getElementById('dom-overlay-container-one');
-  handleComplete();
-
-  function handleComplete() {
-    //This function is always called, irrespective of the content. You can use the variable "stage" after it is created in token create_stage.
-    exportRoot = new libOne.scene1animationv2(); //exportRoot = new window["lib"+index+"."+exportRoot]
-    stage = new createjs.Stage(canvas);
-    stage.addChild(exportRoot);
-    //Registers the "tick" event listener.
-    fnStartAnimation = function() {
-      createjs.Ticker.setFPS(libOne.properties.fps);
-      createjs.Ticker.addEventListener("tick", stage);
-    }
-
-    makeResponsive(false, 'both', false, 1);
-    fnStartAnimation();
-    console.log("Launched One");
-  }
-}
-
-// function configAnimation(index,exportRoot)
-// configAnimation("one","scene1animationv2")
-
-function animationTwo() {
-  canvas = document.getElementById("canvas-two");
-  anim_container = document.getElementById("animation-container-two");
-  dom_overlay_container = document.getElementById("dom-overlay-container-two");
-  handleComplete();
-
-  function handleComplete() {
-    //This function is always called, irrespective of the content. You can use the variable "stage" after it is created in token create_stage.
-    exportRoot = new libTwo.scene2animationv2();
-    stage = new createjs.Stage(canvas);
-    stage.addChild(exportRoot);
-    //Registers the "tick" event listener.
-    fnStartAnimation = function() {
-      createjs.Ticker.setFPS(libTwo.properties.fps);
-      createjs.Ticker.addEventListener("tick", stage);
-    }
-
-    makeResponsive(false, 'both', false, 1);
-    fnStartAnimation();
-    console.log("Launched Two");
-  }
-}
-
-function animationThree() {
-  canvas = document.getElementById("canvas-three");
-  anim_container = document.getElementById("animation-container-three");
-  dom_overlay_container = document.getElementById("dom-overlay-container-three");
-  handleComplete();
-
-  function handleComplete() {
-    //This function is always called, irrespective of the content. You can use the variable "stage" after it is created in token create_stage.
-    exportRoot = new libThree.scene3animationv2();
-    stage = new createjs.Stage(canvas);
-    stage.addChild(exportRoot);
-    //Registers the "tick" event listener.
-    fnStartAnimation = function() {
-      createjs.Ticker.setFPS(libThree.properties.fps);
-      createjs.Ticker.addEventListener("tick", stage);
-    }
-
-    makeResponsive(false, 'both', false, 1);
-    fnStartAnimation();
-    console.log("Launched Three");
-  }
-}
-
-function animationFour() {
-  canvas = document.getElementById("canvas-four");
-  anim_container = document.getElementById("animation-container-four");
-  dom_overlay_container = document.getElementById("dom-overlay-container-four");
-  handleComplete();
-
-  function handleComplete() {
-    //This function is always called, irrespective of the content. You can use the variable "stage" after it is created in token create_stage.
-    exportRoot = new libFour.scene4animationv2();
-    stage = new createjs.Stage(canvas);
-    stage.addChild(exportRoot);
-    //Registers the "tick" event listener.
-    fnStartAnimation = function() {
-      createjs.Ticker.setFPS(libFour.properties.fps);
-      createjs.Ticker.addEventListener("tick", stage);
-    }
-
-    makeResponsive(false, 'both', false, 1);
-    fnStartAnimation();
-    console.log("Launched Four");
   }
 }
