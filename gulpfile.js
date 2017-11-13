842abeda4b187a67bc094df22b53801ca63c1eba
Eric Fuhrmann <ekfuhrmann@gmail.com>
2017-11-13 09:38:21 -0500

    renamed build file to 'dist' from 'public_html'

--- gulpfile.js
+++ gulpfile.js
@@ -7,39 +7,39 @@ const $ = plugins();
 // Images
 gulp.task('images', () => {
   return gulp.src('src/images/**/*')
-    .pipe($.changed('./public_html/images'))
+    .pipe($.changed('./dist/images'))
     .pipe($.imagemin([imageminJpegRecompress()]))
-    .pipe(gulp.dest('./public_html/images'));
+    .pipe(gulp.dest('./dist/images'));
 });
 
 // Docs
 gulp.task('documents', () => {
   return gulp.src('src/documents/**/*')
-    .pipe($.changed('./public_html/images'))
-    .pipe(gulp.dest('./public_html/documents'));
+    .pipe($.changed('./dist/images'))
+    .pipe(gulp.dest('./dist/documents'));
 });
 
 // Fonts
 gulp.task('fonts', () => {
   return gulp.src('src/fonts/**/*')
-    .pipe(gulp.dest('./public_html/fonts'));
+    .pipe(gulp.dest('./dist/fonts'));
 });
 
 // HTML
 gulp.task('ejs', () => {
   return gulp.src(['src/views/*.ejs', 'src/views/!(partials)**/*.ejs'])
-    .pipe($.changed('./public_html'))
+    .pipe($.changed('./dist'))
     .pipe($.ejs())
     .on('error', $.notify.onError('Error: <%= error.message %>'))
     .pipe($.htmlmin({collapseWhitespace: true, removeComments: true}))
     .pipe($.ejs({}, {ext:'.html'}))
-    .pipe(gulp.dest('./public_html'));
+    .pipe(gulp.dest('./dist'));
 });
 
 // CSS
 gulp.task('sass', () => {
   return gulp.src(['src/styles/style.scss', 'src/styles/stretch.scss'])
-    .pipe($.changed('./public_html/styles'))
+    .pipe($.changed('./dist/styles'))
     .pipe($.sass({
         outputStyle: 'compressed',
         includePaths: [
@@ -51,7 +51,7 @@ gulp.task('sass', () => {
     .on('error', $.notify.onError('Error: <%= error.message %>'))
     .pipe($.autoprefixer())
     .pipe($.rename({suffix: '.min'}))
-    .pipe(gulp.dest('./public_html/styles'));
+    .pipe(gulp.dest('./dist/styles'));
 });
 
 // JS
@@ -61,36 +61,36 @@ gulp.task('scripts', () => {
     './node_modules/waypoints/lib/shortcuts/inview.min.js',
     'src/scripts/main.js',
   ])
-    .pipe($.changed('./public_html/scripts'))
+    .pipe($.changed('./dist/scripts'))
     .pipe($.babel({
       presets: ['env'],
       ignore: ['./node_modules/']
     })).on('error', $.notify.onError('Error: <%= error.message %>'))
     .pipe($.concat('scripts.js'))
     .pipe($.uglify())
-    .pipe(gulp.dest('./public_html/scripts'));
+    .pipe(gulp.dest('./dist/scripts'));
 });
 
 // JS ScrollReveal
 gulp.task('scripts-sr', () => {
   return gulp.src('src/scripts/scrollreveal*.js')
-    .pipe($.changed('./public_html/scripts'))
+    .pipe($.changed('./dist/scripts'))
     .pipe($.babel())
     .on('error', $.notify.onError('Error: <%= error.message %>'))
     .pipe($.concat('scrollreveal.min.js'))
   .pipe($.uglify())
-    .pipe(gulp.dest('./public_html/scripts'));
+    .pipe(gulp.dest('./dist/scripts'));
 });
 
 // JS Vivus
 gulp.task('scripts-vivus', () => {
   return gulp.src('src/scripts/vivus*.js')
-    .pipe($.changed('./public_html/scripts'))
+    .pipe($.changed('./dist/scripts'))
     .pipe($.babel())
     .on('error', $.notify.onError('Error: <%= error.message %>'))
     .pipe($.concat('vivus.min.js'))
     .pipe($.uglify())
-    .pipe(gulp.dest('./public_html/scripts'));
+    .pipe(gulp.dest('./dist/scripts'));
 });
 
 // JS Animations
@@ -99,18 +99,18 @@ gulp.task('scripts-animations', () => {
       'src/scripts/animations/approach.js',
       'src/scripts/animations/*.js'
     ])
-    .pipe($.changed('./public_html/scripts'))
+    .pipe($.changed('./dist/scripts'))
     .pipe($.babel())
     .on('error', $.notify.onError('Error: <%= error.message %>'))
     .pipe($.concat('animations.js'))
     .pipe($.uglify())
-    .pipe(gulp.dest('./public_html/scripts'));
+    .pipe(gulp.dest('./dist/scripts'));
 });
 
 // Favicons
 gulp.task('favicons', () => {
   return gulp.src('src/favicons/*')
-    .pipe(gulp.dest('./public_html/'));
+    .pipe(gulp.dest('./dist/'));
 });
 
 // Server files
@@ -120,7 +120,7 @@ gulp.task('server', () => {
     'src/server/.htaccess'
   ])
     .on('error', $.notify.onError('Error: <%= error.message %>'))
-    .pipe(gulp.dest('./public_html/'));
+    .pipe(gulp.dest('./dist/'));
 });
 
 // The main building block task
@@ -147,7 +147,7 @@ function reload(done) {
 gulp.task('browser-sync', done => {
   return browserSync.init({
     server : {
-      baseDir : './public_html/',
+      baseDir : './dist/',
       serveStaticOptions : {
         extensions : ['html']
       }
@@ -173,7 +173,7 @@ gulp.task('watch', done => {
 });
 
 gulp.task('deploy', () => {
-  return gulp.src('./public_html/**/*')
+  return gulp.src('./dist/**/*')
     .pipe($.ghPages());
 });
 
